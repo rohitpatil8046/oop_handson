0d7f1583e434319d9fc8994250ab97235b078812
rohitpatil8046 <106608815+rohitpatil8046@users.noreply.github.com>
2022-06-06 17:38:39 +0530

    Add files via upload

---
+++ Abstraction.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace oops_handson
+{
+    class program
+    {
+        abstract class animal
+        {
+            public abstract void eat();
+            public void sound()
+            {
+                Console.WriteLine("dog can sound");
+            }
+        }
+        class dog : animal
+        {
+            public override void eat()
+            {
+                Console.WriteLine("dog can eat");
+            }
+        }
+        static void Main(string[] args)
+        {
+            dog mydog = new dog();
+            animal thePet = mydog;
+            thePet.eat();
+            mydog.sound();
+            Console.ReadLine();
+        }
+    }
+}
