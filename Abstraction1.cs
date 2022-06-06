e99bf34f57f1d9e6a53652bf788a149d12bcebfe
rohitpatil8046 <106608815+rohitpatil8046@users.noreply.github.com>
2022-06-06 17:42:08 +0530

    Add files via upload

---
+++ Abstraction1.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace oops_handson
+{
+    abstract class Language
+    {
+        public void display()
+        {
+            Console.WriteLine("Non abstract method");
+        }
+    }
+
+   
+    class Program : Language
+    {
+
+        static void Main(string[] args)
+        {
+
+            Program obj = new Program();
+
+            obj.display();
+
+            Console.ReadLine();
+        }
+    }
+}
