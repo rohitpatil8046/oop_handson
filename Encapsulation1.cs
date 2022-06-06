e106537f675e198fb199ab473b7f69ba8b0dbb56
rohitpatil8046 <106608815+rohitpatil8046@users.noreply.github.com>
2022-06-06 17:52:46 +0530

    Add files via upload

---
+++ Encapsulation1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace oops_handson
+{
+    public class Bank
+    {
+        private double balance;
+        public double getBalance()
+        { 
+            return balance;
+        }
+        public void setBalance(double balance)
+        {
+            this.balance = balance;
+        }
+    }
+    class BankUser
+    {
+        public static void Main()
+        {
+            Bank SBI = new Bank();
+            SBI.setBalance(500);
+            Console.WriteLine(SBI.getBalance());
+            Console.WriteLine("Press any key to exist.");
+            Console.ReadKey();
+        }
+    }
+}
