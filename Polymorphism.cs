49ecc554b1502e932af05464108375c1fedef941
rohitpatil8046 <106608815+rohitpatil8046@users.noreply.github.com>
2022-06-06 17:33:12 +0530

    Add files via upload

---
+++ Polymorphism.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace oops_handson
+{
+    class Animal  
+    {
+        public void animalSound()
+        {
+            Console.WriteLine("The animal makes a sound");
+        }
+    }
+
+    class Pig : Animal  
+    {
+        public void animalSound()
+        {
+            Console.WriteLine("The pig says: wee wee");
+        }
+    }
+
+    class Dog : Animal  
+    {
+        public void animalSound()
+        {
+            Console.WriteLine("The dog says: bow wow");
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Animal myAnimal = new Animal();  
+            Animal myPig = new Pig();  
+            Animal myDog = new Dog(); 
+
+            myAnimal.animalSound();
+            myPig.animalSound();
+            myDog.animalSound();
+        }
+    }
+}
