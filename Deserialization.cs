556a63f972be7238317244a423311eeced295be9
rohitpatil8046 <106608815+rohitpatil8046@users.noreply.github.com>
2022-06-07 18:02:00 +0530

    Add files via upload

---
+++ Deserialization.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
+[Serializable]
+class Student
+{
+    public int rollno;
+    public string name;
+    public Student(int rollno, string name)
+    {
+        this.rollno = rollno;
+        this.name = name;
+    }
+}
+public class DeserializeExample
+{
+    public static void Main(string[] args)
+    {
+        FileStream stream = new FileStream("e:\\sss.txt", FileMode.OpenOrCreate);
+        BinaryFormatter formatter = new BinaryFormatter();
+
+        Student s = (Student)formatter.Deserialize(stream);
+        Console.WriteLine("Rollno: " + s.rollno);
+        Console.WriteLine("Name: " + s.name);
+
+        stream.Close();
+    }
+}
