611a9986b2cdb91b71d4210366a40fbfa889aa14
rohitpatil8046 <106608815+rohitpatil8046@users.noreply.github.com>
2022-06-07 14:31:37 +0530

    Add files via upload

---
+++ Shadowing .cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mymethods
+{
+    Public class BaseClass
+{
+    public string GetMethodOwnerName()
+    {
+       return "Base Class";
+    }
+}
+public class ChildClass : BaseClass
+{
+    public new string GetMethodOwnerName()
+    {
+       return "ChildClass";
+    }
+}
+    
+        public static void Main(string[] args)
+        {
+            ChildClass c = new ChildClass();
+            Console.WriteLine(c.GetMethodOwnerName());
+            Console.WriteLine();
+        }
+        
+            
+        
+    }
+}
